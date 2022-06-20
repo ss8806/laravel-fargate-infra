1fbdc60f9863639a1ab5025d401122e807998c0b
ss <evo0622@gmail.com>
2022-06-20 11:47:11 +0900

    deploy

--- envs/prod/cicd/app_foobar/iam.tf
+++ envs/prod/cicd/app_foobar/iam.tf
@@ -2,7 +2,7 @@ resource "aws_iam_user" "github" {
   name = "${local.name_prefix}-${local.service_name}-github"
   
   tags = {
-    Name = "${local.name_prefix}-${local.service_name}-github"
+    yesName = "${local.name_prefix}-${local.service_name}-github"
   }
 }
 
@@ -29,4 +29,13 @@ resource "aws_iam_role" "deployer" {
   tags = {
     Name = "${local.name_prefix}-${local.service_name}-deployer"
   }
+}
+
+data "aws_iam_policy" "ecr_power_user" {
+  arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_role_policy_attachment" "role_deployer_policy_ecr_power_user" {
+  role       = aws_iam_role.deployer.name
+  policy_arn = data.aws_iam_policy.ecr_power_user.arn
 }
